68d7783e7772abef2242b8d5aa6742f8b8f7340a
rkwhr0010 <rkwhr0010@naver.com>
2023-09-07 11:51:41 +0900

    클린코드-함수-Switch문-목록 3-4 작성완료

---
+++ src/chap03/Payroll.java
@@ -0,0 +1,44 @@
+package chap03;
+
+public class Payroll {
+  public Money calculatePay(Employee e){
+    switch (e.type) {
+      case COMMISSIONED:
+        return calculateCommissionedPay(e);
+      case HOURLY:
+        return calculateHourlyPay(e);
+      case SALARIED:
+        return calculateSalariedPay(e);
+      default:
+        throw new InvalidEmployeeType(e.type);
+    }
+  }
+
+  private Money calculateSalariedPay(Employee e) {
+    return null;
+  }
+
+  private Money calculateHourlyPay(Employee e) {
+    return null;
+  }
+
+  private Money calculateCommissionedPay(Employee e) {
+    return null;
+  }
+}
+/*
+ * 테스트 용 더미 클래스
+ */
+class Money {
+
+}
+class Employee{
+  SalraryType type;
+}
+enum SalraryType {
+  COMMISSIONED, HOURLY, SALARIED
+}
+class InvalidEmployeeType extends RuntimeException{
+  InvalidEmployeeType(){}
+  InvalidEmployeeType(SalraryType type){}
+}
