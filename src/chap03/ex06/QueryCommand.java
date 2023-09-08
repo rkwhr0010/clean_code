18f69d4f07e229ef7ce1a669ec501629067998dc
rkwhr0010 <rkwhr0010@naver.com>
2023-09-08 12:41:11 +0900

    클린코드-함수-함수인수-명령과질의분리 완료

--- src/chap03/ex06/QueryCommand.java
+++ src/chap03/ex06/QueryCommand.java
@@ -2,14 +2,14 @@ package chap03.ex06;
 
 public class QueryCommand {
   
-  public boolean set(String attrbute, String value) {
-    //저장에 성공하면 true를 실패하면 false를 반환한다고 가정
+  public void set(String attribute, String value) {
+  }
+  public boolean attributeExists(String string) {
     return false;
   }
-
   public void doSomething() {
-    /* 이상한 코드 형식 */
-    if(set("하나", "둘")) {
+    if(attributeExists("하나")) {
+      set("하나", "둘");
     }
   }
 }
