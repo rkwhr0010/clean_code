f169f38efafdc38d89e1abf0d5f5bfd2ff2bfeb8
rkwhr0010 <rkwhr0010@naver.com>
2023-09-13 12:04:50 +0900

    클린코드-주석-나쁜주석-의무적으로다는주석

--- src/chap04/ex04/ListItemWidget.java
+++ src/chap04/ex04/ListItemWidget.java
@@ -2,4 +2,7 @@ package chap04.ex04;
 
 public class ListItemWidget {
 
+  public ListItemWidget(Exam exam, String listItemContent, int i) {
+  }
+
 }
---
+++ src/chap04/ex06/Exam.java
@@ -0,0 +1,12 @@
+package chap04.ex06;
+
+public class Exam {
+  /**
+   * @param title CD 제목
+   * @param author CD 저자
+   * @param tracks CD 트랙수
+   * @param durationInMinutes CD 분 길이
+   */
+  public void addCd(String title, String author, int tracks, int durationInMinutes) {
+  }
+}
