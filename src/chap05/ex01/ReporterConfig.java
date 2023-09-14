2db7d554ea756f2d20fc78d122214b3bccbc0359
rkwhr0010 <rkwhr0010@naver.com>
2023-09-14 12:29:06 +0900

    클린코드-형식-맞추는목적-세로 밀집도 완료

--- src/chap05/ex01/ReporterConfig.java
+++ src/chap05/ex01/ReporterConfig.java
@@ -4,15 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReporterConfig {
-  /**
-   * 리포터 리스너 클래스 이름
-   */
-  private String m_className;
 
-  /**
-   * 리포터 리스너 속성
-   */
+  private String m_className;
   private List<Property> m_properties = new ArrayList<>();
+  
   public void addProperty(Property property) {
     m_properties.add(property);
   }
