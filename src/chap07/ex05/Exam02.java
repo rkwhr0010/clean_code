fa38eac87b5250c30ebad43cb53b431844f07b8d
rkwhr0010 <rkwhr0010@naver.com>
2023-10-05 12:44:06 +0900

    클린코드-오류처리-null을반환하지마라-완료

--- src/chap07/ex05/Exam02.java
+++ src/chap07/ex05/Exam02.java
@@ -1,19 +1,25 @@
 package chap07.ex05;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Exam02 {
   void exam() {
     int totalPay = 0;
+
     List<Employee> employees = getEmployees();
-    if(employees != null) {
-      for(Employee e : employees) {
-        totalPay += e.getPay();
-      }
+    //null을 체크할 필요가 없다.
+    for(Employee e : employees) {
+      totalPay += e.getPay();
     }
   }
 
   private List<Employee> getEmployees() {
+    boolean result = false; //직원이 없다고 가정
+    if(result) {
+      return Collections.emptyList();
+    }
+    //정상적으로 반환했다고 가정
     return null;
   }
 }
