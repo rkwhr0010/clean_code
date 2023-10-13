7e047ca28b83e80509bbd4167a48198e064579e2
rkwhr0010 <rkwhr0010@naver.com>
2023-10-13 13:07:11 +0900

    클린코드-클래스-작아야한다-단일책임원칙

--- src/chap10/ex02/SuperDashboard.java
+++ src/chap10/ex02/SuperDashboard.java
@@ -7,7 +7,4 @@ import javax.swing.JFrame;
 public class SuperDashboard extends JFrame implements MetaDataUser{
 	public Component getLastFocusedComponent() {return null;}
 	public void setlastFocused(Component component) {}
-	public int getMajorVersionNumber() {return 0;}
-	public int getMinorVersionNumber() {return 0;}
-	public int getBuildNumber() {return 0;}
 }
---
+++ src/chap10/ex02/Version.java
@@ -0,0 +1,7 @@
+package chap10.ex02;
+
+public class Version {
+	public int getMajorVersionNumber() {return 0;}
+	public int getMinorVersionNumber() {return 0;}
+	public int getBuildNumber() {return 0;}
+}
