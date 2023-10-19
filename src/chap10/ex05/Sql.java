1951d9c0bd793b1b87c387095e7327530bfb1b25
rkwhr0010 <rkwhr0010@naver.com>
2023-10-19 12:26:05 +0900

    클린코드-클래스-변경하기쉬운클래스-SRP,OCP준수

--- src/chap10/ex05/Sql.java
+++ src/chap10/ex05/Sql.java
@@ -1,15 +1,71 @@
 package chap10.ex05;
 
-public class Sql {
+abstract public class Sql {
 	public Sql(String table, Column[] columns) {}
-	public String create() {return "";}
-	public String insert(Object[] fields) {return "";}
-	public String selectAll() {return "";}
-	public String findByKey(String keyColumn, String keyValue) {return "";}
-	public String select(Column column, String pattern) {return "";}
-	public String select(Criteria criteria) {return "";}
-	private String columnList(Column[] columns) {return "";}
+	abstract public String generate();
+}
+
+class CreateSql extends Sql {
+	public CreateSql(String table, Column[] columns) {
+		super(table, columns);
+	}
+	@Override public String generate() {return null;}
+}
+
+class SelectSql extends Sql {
+	public SelectSql(String table, Column[] columns) {
+		super(table, columns);
+	}
+	@Override public String generate() {return null;}
+}
+
+class InsertSql extends Sql {
+	public InsertSql(String table, Column[] columns, Object[] fields) {
+		super(table, columns);
+	}
+	@Override public String generate() {return null;}
 	private String valuesList(Object[] fields, final Column[] columns) {return "";}
-	private String selectWithCriteria(String criteria) {return "";}
+}
+
+class SelectWithCriteriaSql extends Sql {
+	public SelectWithCriteriaSql(String table, Column[] columns, 
+			Criteria criteria) {
+		super(table, columns);
+	}
+	@Override public String generate() {return null;}
+}
+
+class SelectWithMatchSql extends Sql {
+	public SelectWithMatchSql(String table, Column[] columns,
+			Column column, String pattern) {
+		super(table, columns);
+	}
+	@Override public String generate() {return null;}
+}
+
+class FindByKeySql extends Sql {
+	public FindByKeySql(String table, Column[] columns,
+			String keyColumn, String keyValue) {
+		super(table, columns);
+	}
+	@Override public String generate() {return null;}
+}
+
+class PreparedInsertSql extends Sql {
+	public PreparedInsertSql(String table, Column[] columns,
+			String keyColumn, String keyValue) {
+		super(table, columns);
+	}
+	@Override public String generate() {return null;}
 	private String placeholderList(Column[] columns) {return "";}
+}
+
+class Where {
+	public Where(String criteria) {	}
+	public String generate() {return "";}
+}
+
+class ColumnList {
+	public ColumnList(Column[] columns) {	}
+	public String generate() {return "";}
 }
