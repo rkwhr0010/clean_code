cc19a00e9de8222fafe4aa1898873f97f2be025d
rkwhr0010 <rkwhr0010@naver.com>
2023-10-26 12:46:21 +0900

    클린코드-창발성-중복을없애라-구현중복

---
+++ src/chap12/ex01/Exam.java
@@ -0,0 +1,17 @@
+package chap12.ex01;
+
+public class Exam {
+	public static void main(String[] args) {
+		
+	}
+}
+class List {
+	private int size;
+	
+	public int size() {
+		return size;
+	}
+	public boolean isEmpty() {
+		return size == 0;
+	}
+}
